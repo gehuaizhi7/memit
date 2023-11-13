7c9e2a4908817ed17f90da110ce155fd188f372f
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2023-11-12 19:40:28 -0500

    Update eval_utils_counterfact.py

--- experiments/py/eval_utils_counterfact.py
+++ experiments/py/eval_utils_counterfact.py
@@ -23,8 +23,6 @@ def compute_rewrite_quality_counterfact(
     model: AutoModelForCausalLM,
     tok: AutoTokenizer,
     record: typing.Dict,
-    snips: AttributeSnippets,
-    vec: TfidfVectorizer,
 ) -> typing.Dict:
     """
     Given a rewritten model, computes generalization and specificity metrics for
@@ -96,28 +94,6 @@ def compute_rewrite_quality_counterfact(
         )
     }
 
-    if snips is not None:
-        # Gather reference texts
-        rel_id = record["requested_rewrite"]["relation_id"]
-        consistency_texts = [x["text"] for x in snips[rel_id][target_new["id"]]]
-        essence_texts = [
-            x["text"]
-            for x in snips[rel_id][target_new["id"]]
-            if x["name"] == record["requested_rewrite"]["subject"]
-        ]
-        assert (
-            len(consistency_texts) > 0
-        ), "Must have consistency texts to evaluate generation"
-        gen_stats = test_generation(
-            model,
-            tok,
-            generation_prompts,
-            consistency_texts,
-            essence_texts,
-            vec,
-        )
-        ret.update(gen_stats)
-
     return ret
 
 
@@ -183,75 +159,3 @@ def test_batch_prediction(
     ], targets_correct
 
 
-def test_generation(
-    model,
-    tok,
-    prefixes: typing.List[str],
-    consistency_texts: typing.List[str],
-    essence_texts: typing.List[str],
-    vec: TfidfVectorizer,
-):
-    gen_texts = generate_fast(
-        model,
-        tok,
-        prefixes,
-        n_gen_per_prompt=1,
-        max_out_len=100,
-    )
-
-    ngram_entropy = n_gram_entropy(gen_texts)
-    consistency_tfidf = tfidf_similarity(
-        " ".join(gen_texts), " ".join(consistency_texts), vec
-    )
-
-    ret = {
-        "ngram_entropy": ngram_entropy,
-        "reference_score": consistency_tfidf,
-        "text": gen_texts,
-    }
-
-    if len(essence_texts) > 0:
-        ppl = perplexity(model, tok, " ".join(essence_texts), max_input_length=100)
-        ret.update({"essence_score": ppl, "essence_text": essence_texts})
-
-    return ret
-
-
-def n_gram_entropy(gen_texts, agg="arith"):
-    assert agg in ["arith", "geom"]
-
-    return (scipy.stats.mstats.gmean if agg == "geom" else np.mean)(
-        [compute_n_gram_entropy(txt) for txt in gen_texts]
-    ).item()
-
-
-def compute_n_gram_entropy(sentence, ns=None, weights=None, agg="arith"):
-    if ns is None:
-        ns = [2, 3]
-    if weights is None:
-        weights = [2 / 3, 4 / 3]
-    assert agg in ["arith", "geom"]
-
-    entropy_list = []
-    for n in ns:
-        fdist = compute_freq(sentence, n)
-        freqs = np.array([freq for _, freq in fdist.items()])
-        freqs = freqs / freqs.sum()
-
-        entropy_list.append(np.sum(-freqs * np.log(freqs) / np.log(2)))
-
-    entropy_list = np.array(entropy_list) * np.array(weights)
-
-    return (scipy.stats.mstats.gmean if agg == "geom" else np.mean)(entropy_list)
-
-
-def compute_freq(sentence, n=2):
-    tokens = nltk.word_tokenize(sentence)
-    ngrams = nltk.ngrams(tokens, n)
-    return nltk.FreqDist(ngrams)
-
-
-def tfidf_similarity(text_a, text_b, vec):
-    encs = vec.transform([text_a, text_b]).A
-    norm = np.linalg.norm
-    return (np.dot(encs[0], encs[1]) / norm(encs[0]) / norm(encs[1])).item()
